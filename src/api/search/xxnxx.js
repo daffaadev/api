c4e8607d4c953968a591f09afeb78e9cede81db5
daffaadev <githubdaffaa@gmail.com>
2026-06-24 02:36:00 +0700

    Update xxnxx.js

--- src/api/search/xxnxx.js
+++ src/api/search/xxnxx.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
       // Pake axios
       const axios = require('axios');
       
-      const response = await axios.get(`https://www.xxxx.com/search/${encodeURIComponent(query)}`, {
+      const response = await axios.get(`https://www.xnxx.com/search/${encodeURIComponent(query)}`, {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
