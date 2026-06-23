c8f173750bc6b6078405ca5a67811669d1e1e7de
daffaadev <githubdaffaa@gmail.com>
2026-06-24 02:26:28 +0700

    Update xxnxx.js

--- src/api/search/xxnxx.js
+++ src/api/search/xxnxx.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
         });
       }
 
-      const response = await fetch(`https://www.xxxx.com/search/${encodeURIComponent(query)}`, {
+      const response = await fetch(`https://xnxx.com/search/${encodeURIComponent(query)}`, {
         method: 'GET',
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
